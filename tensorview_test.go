691e33c41f9b8070a914c53ecd3fbd852528458c
Marco Nicola <marconicola@disroot.org>
2023-06-05 13:50:27 +0200

    Restart from scratch: remove first-implementation source code

--- tensorview_test.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2023 The NLP Odyssey Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package safetensors
-
-var _ View = TensorView{}
