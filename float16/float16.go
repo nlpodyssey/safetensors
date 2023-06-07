79eabaa5c144eb576f3cb89c6290d8318621bd85
Marco Nicola <marconicola@disroot.org>
2023-06-07 19:34:02 +0200

    Add float16 package

---
+++ float16/float16.go
@@ -0,0 +1,13 @@
+// Copyright 2023 The NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package float16
+
+// F16 is a placeholder for a 16-bit half-precision floating-point value,
+// represented as raw bits (uint16).
+type F16 uint16
+
+// BF16 is a placeholder for a 16-bit brain floating-point value,
+// represented as raw bits (uint16).
+type BF16 uint16
